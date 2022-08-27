244fb3ba281e1103f00dd4de7d2abd26f5200031
lazytrainer09 <wihan0927@gmail.com>
2022-08-27 15:56:01 +0900

    section3

--- react-sample/src/components/ContainerSample.tsx
+++ react-sample/src/components/ContainerSample.tsx
@@ -1,4 +1,11 @@
-const Container = (props: { title: string; children: React.ReactElement }) => {
+import React from "react";
+
+type ContainerProps = {
+  title: string
+  children: React.ReactNode
+}
+
+const Container = (props: ContainerProps): JSX.Element => {
   const { title, children } = props
 
   return (
@@ -9,7 +16,7 @@ const Container = (props: { title: string; children: React.ReactElement }) => {
   )
 }
 
-const Parent = () => {
+const Parent = (): JSX.Element => {
   return (
     <Container title="Hello">
       <p>ここの部分が背景色で囲まれます</p>
